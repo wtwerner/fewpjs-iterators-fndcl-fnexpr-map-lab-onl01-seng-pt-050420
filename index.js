d6d77203b2a9926496d8a8a14bc71a14c982b1e5
wtwerner <wtwerner@gmail.com>
2020-11-20 08:51:42 -0600

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,10 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => {
+    let words = tutorial.split(" ").map(word => {
+      return word = word[0].toUpperCase() + word.slice(1)
+    })
+    return words.join(" ")
+  })
 }
